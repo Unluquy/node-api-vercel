55e8f9836c0f0fd465ca78e207c0a7a510ddd459
Nathan Gorecki <goreckinathans@gmail.com>
2023-05-10 09:54:13 +0200

    test

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const app = require('./app');
+//const app = require('./app');
 const config = require('./config/config.js');
 
 const PORT = 5000
 
 console.log(`NODE_ENV=${config.NODE_ENV}`);
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
+
+const express = require('express');
+//const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+
+//Connect Database
+//connectDB();
+
+app.use(cors());
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Define Routes
+app.use('/api/user', require('./routes/user'));
+
+app.use('/api/visites', require('./routes/visites'));
